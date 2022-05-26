1a3adf4b665648deb92d38eb44688038ef2a03c9
MarinhSt <st_marinh@protonmail.com>
2022-05-26 11:41:50 -0500

    Simple states have been added to the UseState and ClassStete components.

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
 class ClassState extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            error: false,
+        }
+    }
     render() {
+        const { error } = this.state
         return (
             <div>
-                <h2>Delete ClassState</h2>
-                <p>Please, insert your security code</p>
+                <h2>Delete {this.props.name}</h2>
+                {!error ? (
+                    <p>Please, insert your security code</p>
+                ) : (
+                    <p>Error: incorrect code</p>
+                )}
                 <input placeholder="security code" />
-                <button>Check it</button>
+                <button onClick={() => this.setState({ error: !error })}>
+                    Check it
+                </button>
             </div>
         )
     }
--- src/UseState.js
+++ src/UseState.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function UseState(params) {
+function UseState({ name }) {
+    const [error, setError] = useState(false)
     return (
         <div>
-            <h2>Delete UseState</h2>
-            <p>Please, insert your security code</p>
+            <h2>Delete {name}</h2>
+            {!error ? (
+                <p>Please, insert your security code</p>
+            ) : (
+                <p>Error: incorrect code</p>
+            )}
+
             <input placeholder="security code" />
-            <button>Check it</button>
+            <button onClick={() => setError(!error)}>Check it</button>
         </div>
     )
 }
