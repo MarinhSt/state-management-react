6b837783745033529e6f2fdb72d75fd3e3874ec4
MarinhSt <st_marinh@protonmail.com>
2022-05-28 11:21:34 -0500

    useEffect used to simulate security code validation in the UseState component.

--- src/UseState.js
+++ src/UseState.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function UseState({ name }) {
     const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    //executes the code when it fulfills the conditions given in the second argument
+    useEffect(() => {
+        //validation to avoid execution on page loading
+        loading &&
+            setTimeout(() => {
+                setLoading(false)
+            }, 1000)
+    }, [loading])
+
     return (
         <div>
             <h2>Delete {name}</h2>
@@ -10,9 +21,10 @@ function UseState({ name }) {
             ) : (
                 <p>Error: incorrect code</p>
             )}
+            {!!loading && <p>Loading...</p>}
 
             <input placeholder="security code" />
-            <button onClick={() => setError(!error)}>Check it</button>
+            <button onClick={() => setLoading(!loading)}>Check it</button>
         </div>
     )
 }
