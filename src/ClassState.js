677759b1d0dd6345f4cdb4d104ad5d5efe52dc74
MarinhSt <st_marinh@protonmail.com>
2022-05-30 20:47:54 -0500

    lifecycle method componentDidUpdate used in the ClassState component to replicate in some way the operation of UseEffect

--- src/ClassState.js
+++ src/ClassState.js
@@ -6,10 +6,22 @@ class ClassState extends React.Component {
 
         this.state = {
             error: false,
+            loading: false,
         }
     }
+
+    // This life cycle method is only called later updating the component.
+    componentDidUpdate() {
+        const { loading } = this.state
+        //validation to avoid execution on page loading
+        loading &&
+            setTimeout(() => {
+                this.setState({ loading: false })
+            }, 1000)
+    }
+
     render() {
-        const { error } = this.state
+        const { error, loading } = this.state
         return (
             <div>
                 <h2>Delete {this.props.name}</h2>
@@ -18,8 +30,9 @@ class ClassState extends React.Component {
                 ) : (
                     <p>Error: incorrect code</p>
                 )}
+                {!!loading && <p>Loading...</p>}
                 <input placeholder="security code" />
-                <button onClick={() => this.setState({ error: !error })}>
+                <button onClick={() => this.setState({ loading: !loading })}>
                     Check it
                 </button>
             </div>
