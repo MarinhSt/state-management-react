35fb8b29b07976510db30ef8cfa42cae17780b76
MarinhSt <st_marinh@protonmail.com>
2022-09-30 13:11:32 -0500

    implemented the complete structure of useReducer to manage state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { UseState } from './UseState.js'
+import { UseReducer } from './UseReducer'
 import { ClassState } from './ClassState.js'
 import './App.css'
 
@@ -7,6 +8,7 @@ function App() {
     return (
         <div className="App">
             <UseState name="Use State" />
+            <UseReducer name="Use Reducer" />
             <ClassState name="Class State" />
         </div>
     )
--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,3 +1,86 @@
+import React, { useEffect } from 'react'
+
+const SECURITY_CODE = 'confirm'
+
+function UseReducer({ name }) {
+    // const [value, setValue] = useState('')
+    // const [error, setError] = useState(false)
+    // const [loading, setLoading] = useState(false)
+
+    //composite state
+    const [state, dispatch] = React.useReducer(reducer, initialState)
+
+    const { value, error, loading, confirmed, deleted } = state
+
+    //executes the code when it fulfills the conditions given in the second argument
+    useEffect(() => {
+        //validation to avoid execution on page loading
+        loading &&
+            setTimeout(() => {
+                //it is necessary to specify that the rest of the properties must also be saved when any of them is changed,
+                //to make sure not to overwrite only this change losing the rest of the states
+                // setState({ ...state, loading: false })
+                value !== SECURITY_CODE
+                    ? dispatch({ type: 'ERROR' })
+                    : dispatch({ type: 'CONFIRM' })
+            }, 1000)
+        console.log(state)
+    }, [loading])
+
+    if (!confirmed && !deleted) {
+        return (
+            <div>
+                <h2>Delete {name}</h2>
+                {!!loading ? (
+                    <p>Loading...</p>
+                ) : !error ? (
+                    <p>Please, insert your security code</p>
+                ) : (
+                    <p>Error: incorrect code</p>
+                )}
+
+                <input
+                    placeholder="security code"
+                    value={value}
+                    onChange={
+                        ({ target }) =>
+                            dispatch({ type: 'WRITE', payload: target.value })
+                        // onWrite(target)
+                    }
+                />
+                <button onClick={() => dispatch({ type: 'CHECK' })}>
+                    Check it
+                </button>
+            </div>
+        )
+    } else if (confirmed) {
+        return (
+            <div>
+                <h2>Delete {name}</h2>
+                <p>Are you sure you want to delete UseState?</p>
+
+                <button onClick={() => dispatch({ type: 'DELETED' })}>
+                    Yes, delete.
+                </button>
+                <button onClick={() => dispatch({ type: 'RECOVER' })}>
+                    No, go back.
+                </button>
+            </div>
+        )
+    } else if (deleted) {
+        return (
+            <div>
+                <h2>{name} was deleted</h2>
+                <p>Do you want to recover UseState?</p>
+
+                <button onClick={() => dispatch({ type: 'RECOVER' })}>
+                    Yes, recover.
+                </button>
+            </div>
+        )
+    }
+}
+
 const initialState = {
     value: '',
     error: false,
@@ -34,7 +117,7 @@ const reducerSwitch = (state, action) => {
 }
 
 // Using a Reducer object - here we separete the object frome the validation
-const reducerObject = state => ({
+const reducerObject = (state, payload) => ({
     ERROR: {
         ...state,
         error: true,
@@ -46,13 +129,34 @@ const reducerObject = state => ({
         loading: false,
         confirmed: true,
     },
+    WRITE: {
+        ...state,
+        value: payload,
+    },
+    CHECK: {
+        ...state,
+        loading: !initialState.loading,
+    },
+    DELETED: {
+        ...state,
+        confirmed: false,
+        deleted: true,
+    },
+    RECOVER: {
+        ...state,
+        value: '',
+        confirmed: false,
+        deleted: false,
+    },
 })
 
 // validation
 const reducer = (state, action) => {
     if (reducerObject(state)[action.type]) {
-        return reducerObject(state)[action.type]
+        return reducerObject(state, action.payload)[action.type]
     } else {
         return state
     }
 }
+
+export { UseReducer }
