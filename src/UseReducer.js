7b29c5d418037731587d5c714dcf254fbf53569d
MarinhSt <st_marinh@protonmail.com>
2022-10-06 11:48:51 -0500

    Using action creators and actionTypes for more declarative state management

--- src/UseReducer.js
+++ src/UseReducer.js
@@ -12,6 +12,17 @@ function UseReducer({ name }) {
 
     const { value, error, loading, confirmed, deleted } = state
 
+    const onError = () => dispatch({ type: actionTypes.ERROR })
+    const onConfirm = () => dispatch({ type: actionTypes.CONFIRM })
+    const onWrite = value =>
+        dispatch({
+            type: actionTypes.WRITE,
+            payload: value,
+        })
+    const onCheck = () => dispatch({ type: actionTypes.CHECK })
+    const onDeleted = () => dispatch({ type: actionTypes.DELETED })
+    const onRecover = () => dispatch({ type: actionTypes.RECOVER })
+
     //executes the code when it fulfills the conditions given in the second argument
     useEffect(() => {
         //validation to avoid execution on page loading
@@ -20,9 +31,7 @@ function UseReducer({ name }) {
                 //it is necessary to specify that the rest of the properties must also be saved when any of them is changed,
                 //to make sure not to overwrite only this change losing the rest of the states
                 // setState({ ...state, loading: false })
-                value !== SECURITY_CODE
-                    ? dispatch({ type: 'ERROR' })
-                    : dispatch({ type: 'CONFIRM' })
+                value !== SECURITY_CODE ? onError() : onConfirm()
             }, 1000)
         console.log(state)
     }, [loading])
@@ -42,15 +51,9 @@ function UseReducer({ name }) {
                 <input
                     placeholder="security code"
                     value={value}
-                    onChange={
-                        ({ target }) =>
-                            dispatch({ type: 'WRITE', payload: target.value })
-                        // onWrite(target)
-                    }
+                    onChange={({ target: { value } }) => onWrite(value)}
                 />
-                <button onClick={() => dispatch({ type: 'CHECK' })}>
-                    Check it
-                </button>
+                <button onClick={() => onCheck()}>Check it</button>
             </div>
         )
     } else if (confirmed) {
@@ -59,12 +62,8 @@ function UseReducer({ name }) {
                 <h2>Delete {name}</h2>
                 <p>Are you sure you want to delete UseState?</p>
 
-                <button onClick={() => dispatch({ type: 'DELETED' })}>
-                    Yes, delete.
-                </button>
-                <button onClick={() => dispatch({ type: 'RECOVER' })}>
-                    No, go back.
-                </button>
+                <button onClick={() => onDeleted()}>Yes, delete.</button>
+                <button onClick={() => onRecover()}>No, go back.</button>
             </div>
         )
     } else if (deleted) {
@@ -73,9 +72,7 @@ function UseReducer({ name }) {
                 <h2>{name} was deleted</h2>
                 <p>Do you want to recover UseState?</p>
 
-                <button onClick={() => dispatch({ type: 'RECOVER' })}>
-                    Yes, recover.
-                </button>
+                <button onClick={() => onRecover()}>Yes, recover.</button>
             </div>
         )
     }
@@ -92,57 +89,67 @@ const initialState = {
 /* different ways to create reducer  */
 
 //Using a classic if else validation
-const reducerIfElse = (state, action) => {
-    if (action.type === 'ERROR') {
-        return { ...state, error: true, loading: false }
-    } else if (action.type === 'CONFIRM') {
-        return { ...state, error: false, loading: false, confirmed: true }
-    } else {
-        /* set default state in case of action.type does not match */
-        return { ...initialState }
-    }
-}
+// const reducerIfElse = (state, action) => {
+//     if (action.type === 'ERROR') {
+//         return { ...state, error: true, loading: false }
+//     } else if (action.type === 'CONFIRM') {
+//         return { ...state, error: false, loading: false, confirmed: true }
+//     } else {
+//         /* set default state in case of action.type does not match */
+//         return { ...initialState }
+//     }
+// }
 
 //using a Switch case validation
-const reducerSwitch = (state, action) => {
-    switch (action.type) {
-        case 'ERROR':
-            return { ...state, error: true, loading: false }
-        //break is't necessary, return execute the same function and finished reducerSwitch function
-        case 'CONFIRM':
-            return { ...state, error: false, loading: false, confirmed: true }
-        default:
-            return { ...initialState }
-    }
+// const reducerSwitch = (state, action) => {
+//     switch (action.type) {
+//         case 'ERROR':
+//             return { ...state, error: true, loading: false }
+//         //break is't necessary, return execute the same function and finished reducerSwitch function
+//         case 'CONFIRM':
+//             return { ...state, error: false, loading: false, confirmed: true }
+//         default:
+//             return { ...initialState }
+//     }
+// }
+
+//is used for stricter validation and easier debugging
+const actionTypes = {
+    ERROR: 'ERROR',
+    CONFIRM: 'CONFIRM',
+    WRITE: 'WRITE',
+    CHECK: 'CHECK',
+    DELETED: 'DELETED',
+    RECOVER: 'RECOVER',
 }
 
 // Using a Reducer object - here we separete the object frome the validation
 const reducerObject = (state, payload) => ({
-    ERROR: {
+    [actionTypes.ERROR]: {
         ...state,
         error: true,
         loading: false,
     },
-    CONFIRM: {
+    [actionTypes.CONFIRM]: {
         ...state,
         error: false,
         loading: false,
         confirmed: true,
     },
-    WRITE: {
+    [actionTypes.WRITE]: {
         ...state,
         value: payload,
     },
-    CHECK: {
+    [actionTypes.CHECK]: {
         ...state,
         loading: !initialState.loading,
     },
-    DELETED: {
+    [actionTypes.DELETED]: {
         ...state,
         confirmed: false,
         deleted: true,
     },
-    RECOVER: {
+    [actionTypes.RECOVER]: {
         ...state,
         value: '',
         confirmed: false,
